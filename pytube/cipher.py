3e90253539ca8c4b6c0a4443c77f9bc00513acc1
1blackghost <ashishjosephnew@gmail.com>
2023-05-10 19:47:15 +0530

    pytube problem fixed

--- pytube/cipher.py
+++ pytube/cipher.py
@@ -1,13 +1,16 @@
 """
 This module contains all logic necessary to decipher the signature.
+
 YouTube's strategy to restrict downloading videos is to send a ciphered version
 of the signature to the client, along with the decryption algorithm obfuscated
 in JavaScript. For the clients to play the videos, JavaScript must take the
 ciphered version, cycle it through a series of "transform functions," and then
 signs the media URL with the output.
+
 This module is responsible for (1) finding and extracting those "transform
 functions" (2) maps them to Python equivalents and (3) taking the ciphered
 signature and decoding it.
+
 """
 import logging
 import re
@@ -72,7 +75,9 @@ class Cipher:
 
     def get_signature(self, ciphered_signature: str) -> str:
         """Decipher the signature.
+
         Taking the ciphered signature, applies the transform functions.
+
         :param str ciphered_signature:
             The ciphered signature sent in the ``player_config``.
         :rtype: str
@@ -101,16 +106,21 @@ class Cipher:
     @cache
     def parse_function(self, js_func: str) -> Tuple[str, int]:
         """Parse the Javascript transform function.
+
         Break a JavaScript transform function down into a two element ``tuple``
         containing the function name and some integer-based argument.
+
         :param str js_func:
             The JavaScript version of the transform function.
         :rtype: tuple
         :returns:
             two element tuple containing the function name and an argument.
+
         **Example**:
+
         parse_function('DE.AJ(a,15)')
         ('AJ', 15)
+
         """
         logger.debug("parsing transform function")
         for pattern in self.js_func_patterns:
@@ -163,11 +173,15 @@ def get_initial_function_name(js: str) -> str:
 
 def get_transform_plan(js: str) -> List[str]:
     """Extract the "transform plan".
+
     The "transform plan" is the functions that the ciphered signature is
     cycled through to obtain the actual signature.
+
     :param str js:
         The contents of the base.js asset file.
+
     **Example**:
+
     ['DE.AJ(a,15)',
     'DE.VR(a,3)',
     'DE.AJ(a,51)',
@@ -185,20 +199,25 @@ def get_transform_plan(js: str) -> List[str]:
 
 def get_transform_object(js: str, var: str) -> List[str]:
     """Extract the "transform object".
+
     The "transform object" contains the function definitions referenced in the
     "transform plan". The ``var`` argument is the obfuscated variable name
     which contains these functions, for example, given the function call
     ``DE.AJ(a,15)`` returned by the transform plan, "DE" would be the var.
+
     :param str js:
         The contents of the base.js asset file.
     :param str var:
         The obfuscated variable name that stores an object with all functions
         that descrambles the signature.
+
     **Example**:
+
     >>> get_transform_object(js, 'DE')
     ['AJ:function(a){a.reverse()}',
     'VR:function(a,b){a.splice(0,b)}',
     'kT:function(a,b){var c=a[0];a[0]=a[b%a.length];a[b]=c}']
+
     """
     pattern = r"var %s={(.*?)};" % re.escape(var)
     logger.debug("getting transform object")
@@ -212,13 +231,16 @@ def get_transform_object(js: str, var: str) -> List[str]:
 
 def get_transform_map(js: str, var: str) -> Dict:
     """Build a transform function lookup.
+
     Build a lookup table of obfuscated JavaScript function names to the
     Python equivalents.
+
     :param str js:
         The contents of the base.js asset file.
     :param str var:
         The obfuscated variable name that stores an object with all functions
         that descrambles the signature.
+
     """
     transform_object = get_transform_object(js, var)
     mapper = {}
@@ -232,6 +254,7 @@ def get_transform_map(js: str, var: str) -> Dict:
 
 def get_throttling_function_name(js: str) -> str:
     """Extract the name of the function that computes the throttling parameter.
+
     :param str js:
         The contents of the base.js asset file.
     :rtype: str
@@ -277,6 +300,7 @@ def get_throttling_function_name(js: str) -> str:
 
 def get_throttling_function_code(js: str) -> str:
     """Extract the raw code for the throttling function.
+
     :param str js:
         The contents of the base.js asset file.
     :rtype: str
@@ -301,6 +325,7 @@ def get_throttling_function_code(js: str) -> str:
 
 def get_throttling_function_array(js: str) -> List[Any]:
     """Extract the "c" array.
+
     :param str js:
         The contents of the base.js asset file.
     :returns:
@@ -366,10 +391,12 @@ def get_throttling_function_array(js: str) -> List[Any]:
 
 def get_throttling_plan(js: str):
     """Extract the "throttling plan".
+
     The "throttling plan" is a list of tuples used for calling functions
     in the c array. The first element of the tuple is the index of the
     function to call, and any remaining elements of the tuple are arguments
     to pass to that function.
+
     :param str js:
         The contents of the base.js asset file.
     :returns:
@@ -381,7 +408,7 @@ def get_throttling_plan(js: str):
     plan_regex = re.compile(transform_start)
     match = plan_regex.search(raw_code)
 
-    transform_plan_raw = js
+    transform_plan_raw = find_object_from_startpoint(raw_code, match.span()[1] - 1)
 
     # Steps are either c[x](c[y]) or c[x](c[y],c[z])
     step_start = r"c\[(\d+)\]\(c\[(\d+)\](,c(\[(\d+)\]))?\)"
@@ -399,12 +426,18 @@ def get_throttling_plan(js: str):
 
 def reverse(arr: List, _: Optional[Any]):
     """Reverse elements in a list.
+
     This function is equivalent to:
+
     .. code-block:: javascript
+
         function(a, b) { a.reverse() }
+
     This method takes an unused ``b`` variable as their transform functions
     universally sent two arguments.
+
     **Example**:
+
     >>> reverse([1, 2, 3, 4])
     [4, 3, 2, 1]
     """
@@ -413,10 +446,15 @@ def reverse(arr: List, _: Optional[Any]):
 
 def splice(arr: List, b: int):
     """Add/remove items to/from a list.
+
     This function is equivalent to:
+
     .. code-block:: javascript
+
         function(a, b) { a.splice(0, b) }
+
     **Example**:
+
     >>> splice([1, 2, 3, 4], 2)
     [1, 2]
     """
@@ -425,10 +463,15 @@ def splice(arr: List, b: int):
 
 def swap(arr: List, b: int):
     """Swap positions at b modulus the list length.
+
     This function is equivalent to:
+
     .. code-block:: javascript
+
         function(a, b) { var c=a[0];a[0]=a[b%a.length];a[b]=c }
+
     **Example**:
+
     >>> swap([1, 2, 3, 4], 2)
     [3, 2, 1, 4]
     """
@@ -438,6 +481,7 @@ def swap(arr: List, b: int):
 
 def throttling_reverse(arr: list):
     """Reverses the input list.
+
     Needs to do an in-place reversal so that the passed list gets changed.
     To accomplish this, we create a reversed copy, and then change each
     indvidual element.
@@ -454,8 +498,10 @@ def throttling_push(d: list, e: Any):
 
 def throttling_mod_func(d: list, e: int):
     """Perform the modular function from the throttling array functions.
+
     In the javascript, the modular operation is as follows:
     e = (e % d.length + d.length) % d.length
+
     We simply translate this to python here.
     """
     return (e % len(d) + len(d)) % len(d)
@@ -463,6 +509,7 @@ def throttling_mod_func(d: list, e: int):
 
 def throttling_unshift(d: list, e: int):
     """Rotates the elements of the list to the right.
+
     In the javascript, the operation is as follows:
     for(e=(e%d.length+d.length)%d.length;e--;)d.unshift(d.pop())
     """
@@ -475,6 +522,7 @@ def throttling_unshift(d: list, e: int):
 
 def throttling_cipher_function(d: list, e: str):
     """This ciphers d with e to generate a new list.
+
     In the javascript, the operation is as follows:
     var h = [A-Za-z0-9-_], f = 96;  // simplified from switch-case loop
     d.forEach(
@@ -508,6 +556,7 @@ def throttling_cipher_function(d: list, e: str):
 
 def throttling_nested_splice(d: list, e: int):
     """Nested splice function in throttling js.
+
     In the javascript, the operation is as follows:
     function(d,e){
         e=(e%d.length+d.length)%d.length;
@@ -521,6 +570,7 @@ def throttling_nested_splice(d: list, e: int):
             )[0]
         )
     }
+
     While testing, all this seemed to do is swap element 0 and e,
     but the actual process is preserved in case there was an edge
     case that was not considered.
@@ -542,6 +592,7 @@ def throttling_nested_splice(d: list, e: int):
 
 def throttling_prepend(d: list, e: int):
     """
+
     In the javascript, the operation is as follows:
     function(d,e){
         e=(e%d.length+d.length)%d.length;
@@ -551,6 +602,7 @@ def throttling_prepend(d: list, e: int):
             }
         )
     }
+
     Effectively, this moves the last e elements of d to the beginning.
     """
     start_len = len(d)
@@ -579,6 +631,7 @@ def throttling_swap(d: list, e: int):
 
 def js_splice(arr: list, start: int, delete_count=None, *items):
     """Implementation of javascript's splice function.
+
     :param list arr:
         Array to splice
     :param int start:
@@ -587,6 +640,7 @@ def js_splice(arr: list, start: int, delete_count=None, *items):
         Number of elements to delete from the array
     :param *items:
         Items to add to the array
+
     Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice  # noqa:E501
     """
     # Special conditions for start value
@@ -619,6 +673,7 @@ def js_splice(arr: list, start: int, delete_count=None, *items):
 
 def map_functions(js_func: str) -> Callable:
     """For a given JavaScript transform function, return the Python equivalent.
+
     :param str js_func:
         The JavaScript version of the transform function.
     """
